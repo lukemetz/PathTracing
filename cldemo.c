d2fe7fe2966659cf340346ef5b9a087527f73646
Luke Metz <luke.metz@students.olin.edu>
2012-11-16 20:52:43 -0500

    basic intersections working. Might be issue with camera perspective/skewing though

--- cldemo.c
+++ cldemo.c
@@ -31,203 +31,211 @@
 #include <math.h>
 
 #define CL_CHECK(_expr)                                                         \
-   do {                                                                         \
-     cl_int _err = _expr;                                                       \
-     if (_err == CL_SUCCESS)                                                    \
-       break;                                                                   \
-     fprintf(stderr, "OpenCL Error: '%s' returned %d!\n", #_expr, (int)_err);   \
-     abort();                                                                   \
-   } while (0)
+ do {                                                                         \
+ 	cl_int _err = _expr;                                                       \
+ 	if (_err == CL_SUCCESS)                                                    \
+ 		break;                                                                   \
+ 	fprintf(stderr, "OpenCL Error: '%s' returned %d!\n", #_expr, (int)_err);   \
+ 	abort();                                                                   \
+ } while (0)
 
 #define CL_CHECK_ERR(_expr)                                                     \
-   ({                                                                           \
-     cl_int _err = CL_INVALID_VALUE;                                            \
-     typeof(_expr) _ret = _expr;                                                \
-     if (_err != CL_SUCCESS) {                                                  \
-       fprintf(stderr, "OpenCL Error: '%s' returned %d!\n", #_expr, (int)_err); \
-       abort();                                                                 \
-     }                                                                          \
-     _ret;                                                                      \
-   })
+ ({                                                                           \
+ 	cl_int _err = CL_INVALID_VALUE;                                            \
+ 	typeof(_expr) _ret = _expr;                                                \
+ 	if (_err != CL_SUCCESS) {                                                  \
+ 		fprintf(stderr, "OpenCL Error: '%s' returned %d!\n", #_expr, (int)_err); \
+ 		abort();                                                                 \
+ 	}                                                                          \
+ 	_ret;                                                                      \
+ })
 
-void pfn_notify(const char *errinfo, const void *private_info, size_t cb, void *user_data)
-{
-	fprintf(stderr, "OpenCL Error (via pfn_notify): %s\n", errinfo);
-}
+ void pfn_notify(const char *errinfo, const void *private_info, size_t cb, void *user_data)
+ {
+ 	fprintf(stderr, "OpenCL Error (via pfn_notify): %s\n", errinfo);
+ }
 
-float clamp(float x)
-{
-  return x<0 ? 0 : x>1 ? 1 : x;
-}
+ float clamp(float x)
+ {
+ 	return x<0 ? 0 : x>1 ? 1 : x;
+ }
 
-int to_int(float val)
-{
-  return (int) (pow(clamp(val),1/2.2)*255+.5);
-}
+ int to_int(float val)
+ {
+ 	return (int) (pow(clamp(val),1/2.2)*255+.5);
+ }
 
-int main(int argc, char **argv)
-{
-	cl_platform_id platforms[100];
-	cl_uint platforms_n = 0;
-	CL_CHECK(clGetPlatformIDs(100, platforms, &platforms_n));
+ int main(int argc, char **argv)
+ {
+ 	cl_platform_id platforms[100];
+ 	cl_uint platforms_n = 0;
+ 	CL_CHECK(clGetPlatformIDs(100, platforms, &platforms_n));
 
-	printf("=== %d OpenCL platform(s) found: ===\n", platforms_n);
-	for (int i=0; i<platforms_n; i++){
-		char buffer[10240];
-		printf("  -- %d --\n", i);
-		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_PROFILE, 10240, buffer, NULL));
-		printf("  PROFILE = %s\n", buffer);
-		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, 10240, buffer, NULL));
-		printf("  VERSION = %s\n", buffer);
-		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, 10240, buffer, NULL));
-		printf("  NAME = %s\n", buffer);
-		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, 10240, buffer, NULL));
-		printf("  VENDOR = %s\n", buffer);
-		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_EXTENSIONS, 10240, buffer, NULL));
-		printf("  EXTENSIONS = %s\n", buffer);
-	}
+ 	printf("=== %d OpenCL platform(s) found: ===\n", platforms_n);
+ 	for (int i=0; i<platforms_n; i++){
+ 		char buffer[10240];
+ 		printf("  -- %d --\n", i);
+ 		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_PROFILE, 10240, buffer, NULL));
+ 		printf("  PROFILE = %s\n", buffer);
+ 		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, 10240, buffer, NULL));
+ 		printf("  VERSION = %s\n", buffer);
+ 		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, 10240, buffer, NULL));
+ 		printf("  NAME = %s\n", buffer);
+ 		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, 10240, buffer, NULL));
+ 		printf("  VENDOR = %s\n", buffer);
+ 		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_EXTENSIONS, 10240, buffer, NULL));
+ 		printf("  EXTENSIONS = %s\n", buffer);
+ 	}
 
-	if (platforms_n == 0)
-		return 1;
+ 	if (platforms_n == 0)
+ 		return 1;
 
-	cl_device_id devices[100];
-	cl_uint devices_n = 0;
+ 	cl_device_id devices[100];
+ 	cl_uint devices_n = 0;
 	// CL_CHECK(clGetDeviceIDs(NULL, CL_DEVICE_TYPE_ALL, 100, devices, &devices_n));
-	CL_CHECK(clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_GPU, 100, devices, &devices_n));
+ 	CL_CHECK(clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_GPU, 100, devices, &devices_n));
 
-	printf("=== %d OpenCL device(s) found on platform:\n", platforms_n);
-	for (int i=0; i<devices_n; i++)
-	{
-		char buffer[10240];
-		cl_uint buf_uint;
-		cl_ulong buf_ulong;
-		printf("  -- %d --\n", i);
-		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_NAME, sizeof(buffer), buffer, NULL));
-		printf("  DEVICE_NAME = %s\n", buffer);
-		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_VENDOR, sizeof(buffer), buffer, NULL));
-		printf("  DEVICE_VENDOR = %s\n", buffer);
-		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_VERSION, sizeof(buffer), buffer, NULL));
-		printf("  DEVICE_VERSION = %s\n", buffer);
-		CL_CHECK(clGetDeviceInfo(devices[i], CL_DRIVER_VERSION, sizeof(buffer), buffer, NULL));
-		printf("  DRIVER_VERSION = %s\n", buffer);
-		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(buf_uint), &buf_uint, NULL));
-		printf("  DEVICE_MAX_COMPUTE_UNITS = %u\n", (unsigned int)buf_uint);
-		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(buf_uint), &buf_uint, NULL));
-		printf("  DEVICE_MAX_CLOCK_FREQUENCY = %u\n", (unsigned int)buf_uint);
-		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(buf_ulong), &buf_ulong, NULL));
-		printf("  DEVICE_GLOBAL_MEM_SIZE = %llu\n", (unsigned long long)buf_ulong);
-	}
+ 	printf("=== %d OpenCL device(s) found on platform:\n", platforms_n);
+ 	for (int i=0; i<devices_n; i++)
+ 	{
+ 		char buffer[10240];
+ 		cl_uint buf_uint;
+ 		cl_ulong buf_ulong;
+ 		printf("  -- %d --\n", i);
+ 		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_NAME, sizeof(buffer), buffer, NULL));
+ 		printf("  DEVICE_NAME = %s\n", buffer);
+ 		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_VENDOR, sizeof(buffer), buffer, NULL));
+ 		printf("  DEVICE_VENDOR = %s\n", buffer);
+ 		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_VERSION, sizeof(buffer), buffer, NULL));
+ 		printf("  DEVICE_VERSION = %s\n", buffer);
+ 		CL_CHECK(clGetDeviceInfo(devices[i], CL_DRIVER_VERSION, sizeof(buffer), buffer, NULL));
+ 		printf("  DRIVER_VERSION = %s\n", buffer);
+ 		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(buf_uint), &buf_uint, NULL));
+ 		printf("  DEVICE_MAX_COMPUTE_UNITS = %u\n", (unsigned int)buf_uint);
+ 		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(buf_uint), &buf_uint, NULL));
+ 		printf("  DEVICE_MAX_CLOCK_FREQUENCY = %u\n", (unsigned int)buf_uint);
+ 		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(buf_ulong), &buf_ulong, NULL));
+ 		printf("  DEVICE_GLOBAL_MEM_SIZE = %llu\n", (unsigned long long)buf_ulong);
+ 	}
 
-	if (devices_n == 0)
-		return 1;
+ 	if (devices_n == 0)
+ 		return 1;
 
-	cl_context context;
-	context = CL_CHECK_ERR(clCreateContext(NULL, 1, devices, &pfn_notify, NULL, &_err));
+ 	cl_context context;
+ 	context = CL_CHECK_ERR(clCreateContext(NULL, 1, devices, &pfn_notify, NULL, &_err));
 
   //dumps contents of kernel.cl into a string (don't need to edit this)
-  FILE *f = fopen("kernel.cl", "rb");
-  fseek(f, 0, SEEK_END);
-  long pos = ftell(f);
-  fseek(f, 0, SEEK_SET);
-  char *program_source = malloc(pos);
-  fread(program_source, pos, 1, f);
-  fclose(f);
+ 	FILE *f = fopen("kernel.cl", "rb");
+ 	fseek(f, 0, SEEK_END);
+ 	long pos = ftell(f);
+ 	fseek(f, 0, SEEK_SET);
+ 	char *program_source = malloc(pos);
+ 	fread(program_source, pos, 1, f);
+ 	fclose(f);
 
-	cl_program program;
+ 	cl_program program;
 
-  program = CL_CHECK_ERR(clCreateProgramWithSource(context, 1, (const char **)&program_source, NULL, &_err));
-	if (clBuildProgram(program, 1, devices, "", NULL, NULL) != CL_SUCCESS) {
-		char buffer[10240];
-		clGetProgramBuildInfo(program, devices[0], CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, NULL);
-		fprintf(stderr, "CL Compilation failed:\n%s", buffer);
-		abort();
-	}
-	CL_CHECK(clUnloadCompiler());
+ 	program = CL_CHECK_ERR(clCreateProgramWithSource(context, 1, (const char **)&program_source, NULL, &_err));
+ 	if (clBuildProgram(program, 1, devices, "", NULL, NULL) != CL_SUCCESS) {
+ 		char buffer[10240];
+ 		clGetProgramBuildInfo(program, devices[0], CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, NULL);
+ 		fprintf(stderr, "CL Compilation failed:\n%s", buffer);
+ 		abort();
+ 	}
+ 	CL_CHECK(clUnloadCompiler());
 
-  int width = 30;
-  int height = 20;
+ 	int width = 320;
+ 	int height = 240;
 
-	cl_mem output_r;
-  	cl_mem output_g;
-  	cl_mem output_b;
+ 	cl_mem random_seeds;
+ 	cl_mem output_r;
+ 	cl_mem output_g;
+ 	cl_mem output_b;
 
-  	output_r = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)*width*height, NULL, &_err));
-	output_g = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)*width*height, NULL, &_err));
-	output_b = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)*width*height, NULL, &_err));
+ 	random_seeds = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int)*width*height, NULL, &_err));
+ 	output_r = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)*width*height, NULL, &_err));
+ 	output_g = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)*width*height, NULL, &_err));
+ 	output_b = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)*width*height, NULL, &_err));
+
 
 	//create an instance of the kernel
-	cl_kernel kernel;
+ 	cl_kernel kernel;
 
 	//inputs are for the path_trace function of the kernel
-	kernel = CL_CHECK_ERR(clCreateKernel(program, "path_trace", &_err));
+ 	kernel = CL_CHECK_ERR(clCreateKernel(program, "path_trace", &_err));
 
 	//sets the arguments of path_trace in order	
-	CL_CHECK(clSetKernelArg(kernel, 0, sizeof(output_r), &output_r));
-	CL_CHECK(clSetKernelArg(kernel, 1, sizeof(output_g), &output_g));
-	CL_CHECK(clSetKernelArg(kernel, 2, sizeof(output_b), &output_b));
-	CL_CHECK(clSetKernelArg(kernel, 3, sizeof(width), &width));
-	CL_CHECK(clSetKernelArg(kernel, 4, sizeof(height), &height));
+	CL_CHECK(clSetKernelArg(kernel, 0, sizeof(random_seeds), &random_seeds));
+ 	CL_CHECK(clSetKernelArg(kernel, 1, sizeof(output_r), &output_r));
+ 	CL_CHECK(clSetKernelArg(kernel, 2, sizeof(output_g), &output_g));
+ 	CL_CHECK(clSetKernelArg(kernel, 3, sizeof(output_b), &output_b));
+ 	CL_CHECK(clSetKernelArg(kernel, 4, sizeof(width), &width));
+ 	CL_CHECK(clSetKernelArg(kernel, 5, sizeof(height), &height));
 
-	cl_command_queue queue;
-	queue = CL_CHECK_ERR(clCreateCommandQueue(context, devices[0], 0, &_err));
+ 	cl_command_queue queue;
+ 	queue = CL_CHECK_ERR(clCreateCommandQueue(context, devices[0], 0, &_err));
+ 	
+ 	//set up random seeds buffer
 
-  	cl_event kernel_completion;
+
+ 	for (int i=0; i < width*height; ++i) {
+ 		int seed = rand();
+ 		CL_CHECK(clEnqueueWriteBuffer(queue, random_seeds, CL_TRUE, i*sizeof(int), sizeof(int), &seed, 0, NULL, NULL));
+ 	}
+
+ 	cl_event kernel_completion;
 
   	//this work group is two dimensional
-  	int work_group_size = 2;
+ 	int work_group_size = 2;
 	//the actual size of the work group is widthxheight
-	size_t global_work_size[2] = { width, height};
+ 	size_t global_work_size[2] = { width, height};
 
 	//this creates the work group
-	CL_CHECK(clEnqueueNDRangeKernel(queue, kernel, work_group_size, NULL, global_work_size, NULL, 0, NULL, &kernel_completion));
-	
+ 	CL_CHECK(clEnqueueNDRangeKernel(queue, kernel, work_group_size, NULL, global_work_size, NULL, 0, NULL, &kernel_completion));
+
 	//kernel either runs when you call clWaitForEvents (below) or it runs when you call 
 	//clEnqueueNDRangeKernel (above), we are not really sure.
-	CL_CHECK(clWaitForEvents(1, &kernel_completion));
-	CL_CHECK(clReleaseEvent(kernel_completion));
-	
+ 	CL_CHECK(clWaitForEvents(1, &kernel_completion));
+ 	CL_CHECK(clReleaseEvent(kernel_completion));
+
 
   	//The following junk creates the image
-	printf("Result:");
+ 	printf("Result:");
 
   	//create the ppm file
-  	FILE *fout = fopen("image.ppm", "w");
+ 	FILE *fout = fopen("image.ppm", "w");
 
   	//sets width and height of ppm
-  	fprintf(fout, "P3\n%d %d\n%d\n", width, height, 255);
+ 	fprintf(fout, "P3\n%d %d\n%d\n", width, height, 255);
 
+	//read the output buffers
+ 	size_t size = width*height*sizeof(float);
+ 	float *out_r = (float *)malloc(size);
+ 	float *out_g = (float *)malloc(size);
+ 	float *out_b = (float *)malloc(size);
+ 	CL_CHECK(clEnqueueReadBuffer(queue, output_r, CL_TRUE, 0, size, out_r, 0, NULL, NULL));
+ 	CL_CHECK(clEnqueueReadBuffer(queue, output_g, CL_TRUE, 0, size, out_g, 0, NULL, NULL));
+ 	CL_CHECK(clEnqueueReadBuffer(queue, output_b, CL_TRUE, 0, size, out_b, 0, NULL, NULL));
 	//iterate through each pixel
-	for (int i=0; i<width*height; i++) {
-                //rgb values of a given pixel		
-		float r,g,b;
-
-		//read the output buffers
-		//(it is ordered already, so you don't need to figure out pixel position for the ppm)
-    		CL_CHECK(clEnqueueReadBuffer(queue, output_r, CL_TRUE, i*sizeof(float), sizeof(float), &r, 0, NULL, NULL));
-		CL_CHECK(clEnqueueReadBuffer(queue, output_g, CL_TRUE, i*sizeof(float), sizeof(float), &g, 0, NULL, NULL));
-		CL_CHECK(clEnqueueReadBuffer(queue, output_b, CL_TRUE, i*sizeof(float), sizeof(float), &b, 0, NULL, NULL));
-
-		//print out to screen the rgb values    		
-		printf("%f %f %f \n", r, g, b);
-
+ 	for (int i=0; i<width*height; i++) {
 		//appends the next pixel to the ppm file (it knows where in the ppm to put it)
-    		fprintf(fout, "%d %d %d ", to_int(r), to_int(g), to_int(b));
-	}
+ 		fprintf(fout, "%d %d %d ", to_int(out_r[i]), to_int(out_g[i]), to_int(out_b[i]));
+ 	}
 
 
 	//boiler plate stuff, you can ignore it (but don't touch!)
 
-	printf("\n");
+ 	printf("\n");
 
-	CL_CHECK(clReleaseMemObject(output_r));
-	CL_CHECK(clReleaseMemObject(output_g));
-	CL_CHECK(clReleaseMemObject(output_b));
+ 	CL_CHECK(clReleaseMemObject(output_r));
+ 	CL_CHECK(clReleaseMemObject(output_g));
+ 	CL_CHECK(clReleaseMemObject(output_b));
 
-	CL_CHECK(clReleaseKernel(kernel));
-	CL_CHECK(clReleaseProgram(program));
-	CL_CHECK(clReleaseContext(context));
-  	free(program_source);
-	return 0;
-}
+ 	CL_CHECK(clReleaseKernel(kernel));
+ 	CL_CHECK(clReleaseProgram(program));
+ 	CL_CHECK(clReleaseContext(context));
+ 	free(program_source);
+ 	return 0;
+ }
 
 
--- kernel.cl
+++ kernel.cl
@@ -13,18 +13,42 @@ typedef struct
   float3 direction;
 } Ray;
 
-//random number generator (currently just returns .5)
-float random()
+typedef struct
 {
-  return .5; 
+  float radius;
+  float3 position;
+  float3 emission;
+  float3 color;
+} Sphere;
+
+//hard code in the scene for now
+__constant Sphere spheres[] = {//Scene: radius, position, emission, color, material
+  //Sphere(1e5, Vec( 1e5+1,40.8,81.6), Vec(),Vec(.75,.25,.25),DIFF),//Left
+  //{1e5, {-1e5+99,40.8,81.6},{0,0,0},{.25,.25,.75}},//Rght
+  //{1e5, {50,40.8, 1e5}, {0,0,0},{.75,.75,.75}},//Back
+  //{1e5, {50,40.8,-1e5+170}, {0,0,0},{0,0,0}},//Frnt
+  //Sphere(1e5, Vec(50, 1e5, 81.6),    Vec(),Vec(.75,.75,.75),DIFF),//Botm
+  //Sphere(1e5, Vec(50,-1e5+81.6,81.6),Vec(),Vec(.75,.75,.75),DIFF),//Top
+  {16.5, {27,16.5,47},{0,0,0},{0.99f, 0.99f, 0.99f}},
+  {16.5, {73,16.5,78}, {0,0,0},{0.99f, 0.99f, 0.99f}},
+  {600, {50,681.6-.27,81.6},{12,12,12},{0,0,0}} //Lite
+};
+
+
+//generates a random value and increments the seed
+__constant static unsigned int mult = 2654435761U;
+inline float random(unsigned int *seed)
+{
+  *seed = (*seed)*mult;
+  return (*seed)/((float)0xFFFFFFFF);
 }
 
 //pseudo random number generator (returns a 2d vector living inside unit square)
-inline float2 tent_distribution()
+inline float2 tent_distribution(unsigned int *seed)
 {
 
   //collect first random value
-  float r = 2 * random();
+  float r = 2 * random(seed);
 
   //create return variable
   float2 ret;
@@ -32,34 +56,80 @@ inline float2 tent_distribution()
   //maps your uniform distribution to a tent distribution
   if (r < 1) {
     ret.x = sqrt(r)-1;
-  } else {
+    } else {
     ret.x = 1-sqrt(2-r);
   }
   
   //collects second random value
-  r = 2 * random();
+  r = 2 * random(seed);
 
   //maps your uniform distribution to a tent distribution
   if (r < 1) {
     ret.y = sqrt(r)-1;
-  } else {
+    } else {
     ret.y = 1-sqrt(2-r);
   }
   return ret;
 }
+inline float sphere_intersect_ray(__constant Sphere *sphere, Ray *ray)
+{
+  float3 op = sphere->position - ray->origin; // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
+  float esp = 1e-4;
+  float b = dot(ray->direction, op);
+  float det = b * b - dot(op, op) + sphere->radius*sphere->radius;
+  
+  
+  
+  if (det > 0) {
+    //return det;
+    det = sqrt(det);
+    float t = b-det;
+    if (t > esp) {
+      return t;
+    }
 
-//this needs to be filled out (put complicated math in here)
+    t = b+det;
+    if (t > esp) {
+      return t;
+    }
+  }
+  return 0;
+}
+
+inline bool intersect(Ray *ray, float *t, int *id)
+{
+  float n=6;//sizeof(spheres);
+  float inf = 1e20;
+  *t = inf;
+
+  for(int i=(int)n; i > 0; --i) {
+    float d = sphere_intersect_ray(&(spheres[i]), ray);
+    if ( d && d < (*t)) {
+      *t = d;
+      *id = i;
+    }
+  }
+  return (*t) < inf;
+}
+
 float3 radiance(Ray * ray, int depth)
 {
-  float3 ret = {.8,.1,.3};
-
+  float3 ret = {0,0,0};
+  float t;
+  int id=0;
+  if (!intersect(ray, &t, &id)) {
+    return ret; 
+  }
+  ret.x = 1;
+  ret.y = 1;
+  ret.z = 1;
   return ret;
 }
 
 //inputs are the five kernel arguments (the first 3 are output buffer pointers)
-__kernel void path_trace(__global float *out_r, __global float *out_g, __global float *out_b, int width, int height)
+__kernel void path_trace(__global int *seeds, __global float *out_r, __global float *out_g, __global float *out_b, int width, int height)
 {
-  
+
   //defines camera position
   float3 origin = {50.0f, 52.0f, 295.6f};
   
@@ -68,14 +138,17 @@ __kernel void path_trace(__global float *out_r, __global float *out_g, __global 
   
   //defines camera vector
   Ray cam = {origin, normalize(direction)};
-	
+
   //returns the position of pixel in the work group
   int x = get_global_id(0);
   int y = get_global_id(1);
 
+  int array_index = x+width*y;
+  int seed = seeds[array_index];
+
   //projects rays through the lens
   float3 cx = width * .5135f / height;
-  float3 cy = normalize( cross( cx, cam.direction ) ) * .5135f;
+  float3 cy = normalize(cross(cx, cam.direction)) * .5135f;
   
   //number of samples being run for the pixel
   int samps = 1;
@@ -88,47 +161,40 @@ __kernel void path_trace(__global float *out_r, __global float *out_g, __global 
   //iterates through subpixels in y direction (2 of these)
   for (int sy = 0; sy < 2; ++sy) {
 
-    //not sure what i does (comment this out later)
-    int i=(height - y - 1) * width + x;
-
     //iterates through subpixels in x direction (2 of these)
     for (int sx = 0; sx < 2; ++sx) {
 
-      //initializes sub_radiance (which is radiance of a subpixel)
+      //initializes radiance of this subpixel
       float3 sub_radiance = 0;
 
       //iterates samps times
       for (int s=0; s<samps; s++){
+        //delta is a random vector (lives inside the unit square)
+        //float2 delta = tent_distribution(&seed);
+        float2 delta = {0,0};
 
-	//what is r? I don't know what this does (comment it out later)
-        float4 r = 0;
+        float3 ray_direction = cx * ( ( (sx + .5f + delta.x)/2 + pixel.x)/width - .5f)  +
+                    cy * ( ( (sy + .5f + delta.y)/2 + pixel.y)/height - .5f) + cam.direction;
 
-	//delta is a random vector (lives inside the unit square)
-        float2 delta = tent_distribution();
+        //float3 ray_direction = {-1, -1, 0};
 
-	//what is d? it is used, but I'm not quite sure what it does
-        float3 d = cx * ( ( ( sx + .5f + delta.x) / 2 + pixel.x) 
-              / width) - .5f +
-                   cy * ( ( ( sy + .5f + delta.y) / 2 + pixel.y) 
-              / height) - .5f + cam.direction;
+        //ray is a vector starting at the camera position + d*140
+        //pointing in the direction of d
+        Ray ray = { cam.origin + ray_direction * 140, normalize( ray_direction ) };
 
-	//ray is a vector starting at the camera position + d*140
-	//pointing in the direction of d
-        Ray ray = { cam.origin + d * 140, normalize( d ) };
-
-	//calculate the radiance value for the subpixel for this sample
-	//then average them across samples
+        //calculate the radiance value for the subpixel for this sample
+        //then average them across samples
         sub_radiance += radiance( &ray, 0 ) * ( 1.0f / samps);
       }
 
-	//average the radiance values across subpixels
-	//to find the radiance value of a given pixel
-        final_radiance +=sub_radiance*.25f;// clamp(sub_radiance, 0.f, 1.0f)*.25f;
+      //average the radiance values across subpixels
+      //to find the radiance value of a given pixel
+        final_radiance += sub_radiance*.25f;// clamp(sub_radiance, 0.f, 1.0f)*.25f;
+      }
     }
-  }
 
-  //places result in the correct place within the output buffer
-  out_r[x+width*y] = final_radiance.x;
-  out_g[x+width*y] = final_radiance.y;
-  out_b[x+width*y] = final_radiance.z;
+    //places result in the correct place within the output buffer
+  out_r[array_index] = final_radiance.x;
+  out_g[array_index] = final_radiance.y;
+  out_b[array_index] = final_radiance.z;
 }
