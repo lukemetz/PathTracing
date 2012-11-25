327386571292a04c64bd45e6910456ef2a13924c
Luke Metz <luke.metz@students.olin.edu>
2012-11-25 15:50:15 -0500

    removed bad spacing.

--- cldemo.c
+++ cldemo.c
@@ -30,6 +30,7 @@
 
 #include <math.h>
 
+
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 
 #define CL_CHECK(_expr)                                                         \
@@ -50,215 +51,215 @@
  		abort();                                                                 \
  	}                                                                          \
  	_ret;                                                                      \
- })
+})
 
- void pfn_notify(const char *errinfo, const void *private_info, size_t cb, void *user_data)
- {
- 	fprintf(stderr, "OpenCL Error (via pfn_notify): %s\n", errinfo);
- }
+void pfn_notify(const char *errinfo, const void *private_info, size_t cb, void *user_data)
+{
+	fprintf(stderr, "OpenCL Error (via pfn_notify): %s\n", errinfo);
+}
 
- float clamp(float x)
- {
- 	return x<0 ? 0 : x>1 ? 1 : x;
- }
+float clamp(float x)
+{
+	return x<0 ? 0 : x>1 ? 1 : x;
+}
 
- int to_int(float val)
- {
- 	return (int) (pow(clamp(val),1/2.2)*255+.5);
- }
-
- int main(int argc, char **argv)
- {
- 	cl_platform_id platforms[100];
- 	cl_uint platforms_n = 0;
- 	CL_CHECK(clGetPlatformIDs(100, platforms, &platforms_n));
-
- 	printf("=== %d OpenCL platform(s) found: ===\n", platforms_n);
- 	for (int i=0; i<platforms_n; i++){
- 		char buffer[10240];
- 		printf("  -- %d --\n", i);
- 		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_PROFILE, 10240, buffer, NULL));
- 		printf("  PROFILE = %s\n", buffer);
- 		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, 10240, buffer, NULL));
- 		printf("  VERSION = %s\n", buffer);
- 		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, 10240, buffer, NULL));
- 		printf("  NAME = %s\n", buffer);
- 		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, 10240, buffer, NULL));
- 		printf("  VENDOR = %s\n", buffer);
- 		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_EXTENSIONS, 10240, buffer, NULL));
- 		printf("  EXTENSIONS = %s\n", buffer);
- 	}
-
- 	if (platforms_n == 0)
- 		return 1;
-
- 	cl_device_id devices[100];
- 	cl_uint devices_n = 0;
-	// CL_CHECK(clGetDeviceIDs(NULL, CL_DEVICE_TYPE_ALL, 100, devices, &devices_n));
- 	CL_CHECK(clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_GPU, 100, devices, &devices_n));
-
- 	printf("=== %d OpenCL device(s) found on platform:\n", platforms_n);
- 	for (int i=0; i<devices_n; i++)
- 	{
- 		char buffer[10240];
- 		cl_uint buf_uint;
- 		cl_ulong buf_ulong;
- 		printf("  -- %d --\n", i);
- 		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_NAME, sizeof(buffer), buffer, NULL));
- 		printf("  DEVICE_NAME = %s\n", buffer);
- 		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_VENDOR, sizeof(buffer), buffer, NULL));
- 		printf("  DEVICE_VENDOR = %s\n", buffer);
- 		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_VERSION, sizeof(buffer), buffer, NULL));
- 		printf("  DEVICE_VERSION = %s\n", buffer);
- 		CL_CHECK(clGetDeviceInfo(devices[i], CL_DRIVER_VERSION, sizeof(buffer), buffer, NULL));
- 		printf("  DRIVER_VERSION = %s\n", buffer);
- 		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(buf_uint), &buf_uint, NULL));
- 		printf("  DEVICE_MAX_COMPUTE_UNITS = %u\n", (unsigned int)buf_uint);
- 		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(buf_uint), &buf_uint, NULL));
- 		printf("  DEVICE_MAX_CLOCK_FREQUENCY = %u\n", (unsigned int)buf_uint);
- 		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(buf_ulong), &buf_ulong, NULL));
- 		printf("  DEVICE_GLOBAL_MEM_SIZE = %llu\n", (unsigned long long)buf_ulong);
- 	}
-
- 	if (devices_n == 0)
- 		return 1;
-
- 	cl_context context;
- 	context = CL_CHECK_ERR(clCreateContext(NULL, 1, devices, &pfn_notify, NULL, &_err));
-
-  //dumps contents of kernel.cl into a string (don't need to edit this)
- 	FILE *f = fopen("kernel.cl", "rb");
- 	fseek(f, 0, SEEK_END);
- 	long pos = ftell(f);
- 	fseek(f, 0, SEEK_SET);
- 	char *program_source = malloc(pos);
- 	fread(program_source, pos, 1, f);
- 	fclose(f);
-
- 	cl_program program;
-
- 	program = CL_CHECK_ERR(clCreateProgramWithSource(context, 1, (const char **)&program_source, NULL, &_err));
- 	if (clBuildProgram(program, 1, devices, "", NULL, NULL) != CL_SUCCESS) {
- 		char buffer[10240];
- 		clGetProgramBuildInfo(program, devices[0], CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, NULL);
- 		fprintf(stderr, "CL Compilation failed:\n%s", buffer);
- 		abort();
- 	}
- 	CL_CHECK(clUnloadCompiler());
-
- 	int width = 1024/2;
- 	int height = 768/2;
- 	width=height=200;
- 	cl_mem random_seeds;
- 	cl_mem output_r;
- 	cl_mem output_g;
- 	cl_mem output_b;
- 	#define MAX_WORKGROUP 10000//(1048576/2) //2^20
-
- 	random_seeds = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int)*MAX_WORKGROUP, NULL, &_err));
- 	output_r = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)*MAX_WORKGROUP, NULL, &_err));
- 	output_g = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)*MAX_WORKGROUP, NULL, &_err));
- 	output_b = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)*MAX_WORKGROUP, NULL, &_err));
+int to_int(float val)
+{
+	return (int) (pow(clamp(val),1/2.2)*255+.5);
+}
 
 
-	//create an instance of the kernel
- 	cl_kernel kernel;
 
-	//inputs are for the path_trace function of the kernel
- 	kernel = CL_CHECK_ERR(clCreateKernel(program, "path_trace", &_err));
+int main(int argc, char **argv)
+{
+	cl_platform_id platforms[100];
+	cl_uint platforms_n = 0;
+	CL_CHECK(clGetPlatformIDs(100, platforms, &platforms_n));
 
- 	cl_command_queue queue;
- 	queue = CL_CHECK_ERR(clCreateCommandQueue(context, devices[0], 0, &_err));
-
- 	//set up random seeds buffer
- 	//unsigned int mult = 2654435761U;
- 	unsigned int seed = 123456778;
- 	printf("calculating random seeds \n");
- 	for (int i=0; i < MAX_WORKGROUP; ++i) {
- 		seed = rand();//seed*mult;
- 		CL_CHECK(clEnqueueWriteBuffer(queue, random_seeds, CL_TRUE, i*sizeof(int), sizeof(int), &seed, 0, NULL, NULL));
- 	}
- 	printf("done random calculation \n");
-
- 	cl_event kernel_completion;
-	clock_t time_after;
-	clock_t time_before;
-	time_before = clock();
-
-	//sets the arguments of path_trace in order
-
-	CL_CHECK(clSetKernelArg(kernel, 0, sizeof(random_seeds), &random_seeds));
- 	CL_CHECK(clSetKernelArg(kernel, 1, sizeof(output_r), &output_r));
- 	CL_CHECK(clSetKernelArg(kernel, 2, sizeof(output_g), &output_g));
- 	CL_CHECK(clSetKernelArg(kernel, 3, sizeof(output_b), &output_b));
- 	CL_CHECK(clSetKernelArg(kernel, 4, sizeof(width), &width));
- 	CL_CHECK(clSetKernelArg(kernel, 5, sizeof(height), &height));
-
-  	//this work group is two dimensional
- 	int work_group_size = 1;
-	//the actual size of the work group is widthxheight
- 	size_t total_size = width*height*sizeof(float);
- 	float *out_r = (float *)malloc(total_size);
- 	float *out_g = (float *)malloc(total_size);
- 	float *out_b = (float *)malloc(total_size);
- 	for(int i=0; i < (width*height)/MAX_WORKGROUP+1; ++i) {
- 		size_t workgroup_amount = min(width*height-i*MAX_WORKGROUP, MAX_WORKGROUP);
- 		printf("%d \n", (int)workgroup_amount);
- 		if(workgroup_amount <= 0)
- 			break;
-
- 		size_t global_work_size[1] = { workgroup_amount };
-		//this creates the work group
-		fprintf(stderr, "enqueueing \n");
-		int offset = i*MAX_WORKGROUP;
-		CL_CHECK(clSetKernelArg(kernel, 6, sizeof(offset), &offset));
-
-	 	CL_CHECK(clEnqueueNDRangeKernel(queue, kernel, work_group_size, NULL, global_work_size, NULL, 0, NULL, &kernel_completion));
-	 	fprintf(stderr,"waiting \n");
-		//kernel either runs when you call clWaitForEvents (below) or it runs when you call
-		//clEnqueueNDRangeKernel (above), we are not really sure.
-	 	CL_CHECK(clWaitForEvents(1, &kernel_completion));
-	 	//printf("releasing \n");
-
-
- 		CL_CHECK(clEnqueueReadBuffer(queue, output_r, CL_TRUE, 0, sizeof(float)*workgroup_amount, out_r+i*MAX_WORKGROUP, 0, NULL, NULL));
- 		CL_CHECK(clEnqueueReadBuffer(queue, output_g, CL_TRUE, 0, sizeof(float)*workgroup_amount, out_g+i*MAX_WORKGROUP, 0, NULL, NULL));
- 		CL_CHECK(clEnqueueReadBuffer(queue, output_b, CL_TRUE, 0, sizeof(float)*workgroup_amount, out_b+i*MAX_WORKGROUP, 0, NULL, NULL));
+	printf("=== %d OpenCL platform(s) found: ===\n", platforms_n);
+	for (int i=0; i<platforms_n; i++){
+		char buffer[10240];
+		printf("  -- %d --\n", i);
+		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_PROFILE, 10240, buffer, NULL));
+		printf("  PROFILE = %s\n", buffer);
+		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, 10240, buffer, NULL));
+		printf("  VERSION = %s\n", buffer);
+		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, 10240, buffer, NULL));
+		printf("  NAME = %s\n", buffer);
+		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, 10240, buffer, NULL));
+		printf("  VENDOR = %s\n", buffer);
+		CL_CHECK(clGetPlatformInfo(platforms[i], CL_PLATFORM_EXTENSIONS, 10240, buffer, NULL));
+		printf("  EXTENSIONS = %s\n", buffer);
 	}
-	time_after = clock();
-	CL_CHECK(clReleaseEvent(kernel_completion));
-  	//The following junk creates the image
- 	printf("Done in %f seconds", ((float)(time_after-time_before))/CLOCKS_PER_SEC);
 
-  	//create the ppm file
- 	FILE *fout = fopen("image.ppm", "w");
+	if (platforms_n == 0)
+		return 1;
 
-  	//sets width and height of ppm
- 	fprintf(fout, "P3\n%d %d\n%d\n", width, height, 255);
+	cl_device_id devices[100];
+	cl_uint devices_n = 0;
+// CL_CHECK(clGetDeviceIDs(NULL, CL_DEVICE_TYPE_ALL, 100, devices, &devices_n));
+	CL_CHECK(clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_GPU, 100, devices, &devices_n));
 
-	//read the output buffers
+	printf("=== %d OpenCL device(s) found on platform:\n", platforms_n);
+	for (int i=0; i<devices_n; i++)
+	{
+		char buffer[10240];
+		cl_uint buf_uint;
+		cl_ulong buf_ulong;
+		printf("  -- %d --\n", i);
+		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_NAME, sizeof(buffer), buffer, NULL));
+		printf("  DEVICE_NAME = %s\n", buffer);
+		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_VENDOR, sizeof(buffer), buffer, NULL));
+		printf("  DEVICE_VENDOR = %s\n", buffer);
+		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_VERSION, sizeof(buffer), buffer, NULL));
+		printf("  DEVICE_VERSION = %s\n", buffer);
+		CL_CHECK(clGetDeviceInfo(devices[i], CL_DRIVER_VERSION, sizeof(buffer), buffer, NULL));
+		printf("  DRIVER_VERSION = %s\n", buffer);
+		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(buf_uint), &buf_uint, NULL));
+		printf("  DEVICE_MAX_COMPUTE_UNITS = %u\n", (unsigned int)buf_uint);
+		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(buf_uint), &buf_uint, NULL));
+		printf("  DEVICE_MAX_CLOCK_FREQUENCY = %u\n", (unsigned int)buf_uint);
+		CL_CHECK(clGetDeviceInfo(devices[i], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(buf_ulong), &buf_ulong, NULL));
+		printf("  DEVICE_GLOBAL_MEM_SIZE = %llu\n", (unsigned long long)buf_ulong);
+	}
+
+	if (devices_n == 0)
+		return 1;
+
+	cl_context context;
+	context = CL_CHECK_ERR(clCreateContext(NULL, 1, devices, &pfn_notify, NULL, &_err));
+
+//dumps contents of kernel.cl into a string (don't need to edit this)
+	FILE *f = fopen("kernel.cl", "rb");
+	fseek(f, 0, SEEK_END);
+	long pos = ftell(f);
+	fseek(f, 0, SEEK_SET);
+	char *program_source = malloc(pos);
+	fread(program_source, pos, 1, f);
+	fclose(f);
+
+	cl_program program;
+
+	program = CL_CHECK_ERR(clCreateProgramWithSource(context, 1, (const char **)&program_source, NULL, &_err));
+	if (clBuildProgram(program, 1, devices, "", NULL, NULL) != CL_SUCCESS) {
+		char buffer[10240];
+		clGetProgramBuildInfo(program, devices[0], CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, NULL);
+		fprintf(stderr, "CL Compilation failed:\n%s", buffer);
+		abort();
+	}
+	CL_CHECK(clUnloadCompiler());
+
+	int width = 1024/2;
+	int height = 768/2;
+	width=height=200;
+	cl_mem random_seeds;
+	cl_mem output_r;
+	cl_mem output_g;
+	cl_mem output_b;
+	#define MAX_WORKGROUP 10000//(1048576/2) //2^20
+
+	random_seeds = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int)*MAX_WORKGROUP, NULL, &_err));
+	output_r = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)*MAX_WORKGROUP, NULL, &_err));
+	output_g = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)*MAX_WORKGROUP, NULL, &_err));
+	output_b = CL_CHECK_ERR(clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)*MAX_WORKGROUP, NULL, &_err));
 
 
-	//iterate through each pixel
- 	for (int i=0; i<width*height; i++) {
-		//appends the next pixel to the ppm file (it knows where in the ppm to put it)
- 		fprintf(fout, "%d %d %d ", to_int(out_r[i]), to_int(out_g[i]), to_int(out_b[i]));
- 	}
+//create an instance of the kernel
+	cl_kernel kernel;
+
+//inputs are for the path_trace function of the kernel
+	kernel = CL_CHECK_ERR(clCreateKernel(program, "path_trace", &_err));
+
+	cl_command_queue queue;
+	queue = CL_CHECK_ERR(clCreateCommandQueue(context, devices[0], 0, &_err));
+
+	//set up random seeds buffer
+	//unsigned int mult = 2654435761U;
+	unsigned int seed = 123456778;
+	printf("calculating random seeds \n");
+	for (int i=0; i < MAX_WORKGROUP; ++i) {
+		seed = rand();//seed*mult;
+		CL_CHECK(clEnqueueWriteBuffer(queue, random_seeds, CL_TRUE, i*sizeof(int), sizeof(int), &seed, 0, NULL, NULL));
+	}
+	printf("done random calculation \n");
+
+	cl_event kernel_completion;
+clock_t time_after;
+clock_t time_before;
+time_before = clock();
+
+//sets the arguments of path_trace in order
+
+CL_CHECK(clSetKernelArg(kernel, 0, sizeof(random_seeds), &random_seeds));
+	CL_CHECK(clSetKernelArg(kernel, 1, sizeof(output_r), &output_r));
+	CL_CHECK(clSetKernelArg(kernel, 2, sizeof(output_g), &output_g));
+	CL_CHECK(clSetKernelArg(kernel, 3, sizeof(output_b), &output_b));
+	CL_CHECK(clSetKernelArg(kernel, 4, sizeof(width), &width));
+	CL_CHECK(clSetKernelArg(kernel, 5, sizeof(height), &height));
+
+	//this work group is two dimensional
+	int work_group_size = 1;
+//the actual size of the work group is widthxheight
+	size_t total_size = width*height*sizeof(float);
+	float *out_r = (float *)malloc(total_size);
+	float *out_g = (float *)malloc(total_size);
+	float *out_b = (float *)malloc(total_size);
+	for(int i=0; i < (width*height)/MAX_WORKGROUP+1; ++i) {
+		size_t workgroup_amount = min(width*height-i*MAX_WORKGROUP, MAX_WORKGROUP);
+		printf("%d \n", (int)workgroup_amount);
+		if(workgroup_amount <= 0)
+			break;
+
+		size_t global_work_size[1] = { workgroup_amount };
+	//this creates the work group
+	fprintf(stderr, "enqueueing \n");
+	int offset = i*MAX_WORKGROUP;
+	CL_CHECK(clSetKernelArg(kernel, 6, sizeof(offset), &offset));
+
+ 	CL_CHECK(clEnqueueNDRangeKernel(queue, kernel, work_group_size, NULL, global_work_size, NULL, 0, NULL, &kernel_completion));
+ 	fprintf(stderr,"waiting \n");
+	//kernel either runs when you call clWaitForEvents (below) or it runs when you call
+	//clEnqueueNDRangeKernel (above), we are not really sure.
+ 	CL_CHECK(clWaitForEvents(1, &kernel_completion));
+ 	//printf("releasing \n");
 
 
-	//boiler plate stuff, you can ignore it (but don't touch!)
+		CL_CHECK(clEnqueueReadBuffer(queue, output_r, CL_TRUE, 0, sizeof(float)*workgroup_amount, out_r+i*MAX_WORKGROUP, 0, NULL, NULL));
+		CL_CHECK(clEnqueueReadBuffer(queue, output_g, CL_TRUE, 0, sizeof(float)*workgroup_amount, out_g+i*MAX_WORKGROUP, 0, NULL, NULL));
+		CL_CHECK(clEnqueueReadBuffer(queue, output_b, CL_TRUE, 0, sizeof(float)*workgroup_amount, out_b+i*MAX_WORKGROUP, 0, NULL, NULL));
+}
+time_after = clock();
+CL_CHECK(clReleaseEvent(kernel_completion));
+	//The following junk creates the image
+	printf("Done in %f seconds", ((float)(time_after-time_before))/CLOCKS_PER_SEC);
 
- 	printf("\n");
- 	CL_CHECK(clReleaseMemObject(random_seeds));
- 	CL_CHECK(clReleaseMemObject(output_r));
- 	CL_CHECK(clReleaseMemObject(output_g));
- 	CL_CHECK(clReleaseMemObject(output_b));
+	//create the ppm file
+	FILE *fout = fopen("image.ppm", "w");
 
- 	CL_CHECK(clReleaseKernel(kernel));
- 	CL_CHECK(clReleaseProgram(program));
- 	CL_CHECK(clReleaseContext(context));
- 	free(program_source);
- 	return 0;
- }
+	//sets width and height of ppm
+	fprintf(fout, "P3\n%d %d\n%d\n", width, height, 255);
+
+//read the output buffers
 
 
+//iterate through each pixel
+	for (int i=0; i<width*height; i++) {
+	//appends the next pixel to the ppm file (it knows where in the ppm to put it)
+		fprintf(fout, "%d %d %d ", to_int(out_r[i]), to_int(out_g[i]), to_int(out_b[i]));
+	}
+
+
+//boiler plate stuff, you can ignore it (but don't touch!)
+
+	printf("\n");
+	CL_CHECK(clReleaseMemObject(random_seeds));
+	CL_CHECK(clReleaseMemObject(output_r));
+	CL_CHECK(clReleaseMemObject(output_g));
+	CL_CHECK(clReleaseMemObject(output_b));
+
+	CL_CHECK(clReleaseKernel(kernel));
+	CL_CHECK(clReleaseProgram(program));
+	CL_CHECK(clReleaseContext(context));
+	free(program_source);
+	return 0;
+}
